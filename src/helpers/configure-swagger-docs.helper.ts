d853630fe3d8c06f95299afa217216857a9ec0ac
abdur-shobur <abdur.shobur.me@gmail.com>
2025-09-16 22:22:53 +0600

    fix user auth

--- src/helpers/configure-swagger-docs.helper.ts
+++ src/helpers/configure-swagger-docs.helper.ts
@@ -3,11 +3,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function configureSwaggerDocs(app: INestApplication) {
   if (process.env.NODE_ENV !== 'production') {
+    const port = process.env.SERVER_PORT || '3000';
+    const serverUrl = `http://localhost:${port}`;
     const config = new DocumentBuilder()
       .setTitle('API')
       .setDescription('The API description')
       .setVersion('1.0')
-      .addServer('http://localhost:3000', 'Local server')
+      .addServer(serverUrl, 'Local server')
       .addTag('auth', 'API by authentication')
       .addTag('users', 'API by users management')
       .addTag('app', 'API of example ( resources protected and public ) ')
